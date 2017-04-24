c0a077c8b3e0730e1664f9b6aab2def668c0d422
DoVietVuong-K58CA <v2kt95@gmail.com>
2017-04-24 22:19:52 +0700

    Update Recipes_sql_statement.sql

--- Q2.Database Design/Recipes_sql_statement.sql
+++ Q2.Database Design/Recipes_sql_statement.sql
@@ -8,19 +8,19 @@ CREATE TABLE Recipe (
     ID int NOT NULL PRIMARY KEY,
 	--Name of Recipe
     Name varchar(50) NOT NULL,
-	--Intructions of recipe
-	Intructions TEXT NOT NULL,
-	--ID of Menu store recipe
-	MenuID int
+    --Intructions of recipe
+    Intructions TEXT NOT NULL,
+    --ID of Menu store recipe
+    MenuID int
 );
 
 CREATE TABLE Recipe_Ingredient (
     ID int NOT NULL PRIMARY KEY,
 	--id of recipe and ingredient that recipe have ingredient
     RecipeID int,
-	IngredientID int,
-	--quantity of ingredient
-	Quantity varchar(50)
+    IngredientID int,
+    --quantity of ingredient
+    Quantity varchar(50)
 );
 
 CREATE TABLE Ingredient (
