3c193da0425c8a6c6e1f036cc5dafcbee29650ba
VIETVUONG\Vuong <v2kt95@gmail.com>
2017-04-24 20:09:08 +0700

    add content of respository

---
+++ Q2.Database Design/Recipes_sql_statement.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Menu (
+    ID int NOT NULL PRIMARY KEY,
+	--Name of Menu
+    Name varchar(50) NOT NULL
+);
+
+CREATE TABLE Recipe (
+    ID int NOT NULL PRIMARY KEY,
+	--Name of Recipe
+    Name varchar(50) NOT NULL,
+	--Intructions of recipe
+	Intructions TEXT NOT NULL,
+	--ID of Menu store recipe
+	MenuID int
+);
+
+CREATE TABLE Recipe_Ingredient (
+    ID int NOT NULL PRIMARY KEY,
+	--id of recipe and ingredient that recipe have ingredient
+    RecipeID int,
+	IngredientID int,
+	--quantity of ingredient
+	Quantity varchar(50)
+);
+
+CREATE TABLE Ingredient (
+    ID int NOT NULL PRIMARY KEY,
+	--name of ingredient
+    Name varchar(50) NOT NULL
+);
